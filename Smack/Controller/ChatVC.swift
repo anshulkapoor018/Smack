4bc08b6a9f52635b05288107af66ea281f9379e9
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-10-12 00:50:21 +0530

    FirstCommit

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Anshul Kapoor on 11/10/17.
+//  Copyright © 2017 Anshul Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
