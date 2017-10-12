d85ea1779c47e34b3b384fe8c8b765a83f42fb2e
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-10-12 20:04:40 +0530

    LoginPageFinal

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Anshul Kapoor on 12/10/17.
+//  Copyright © 2017 Anshul Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated:  true, completion: nil)
+    }
+    
+}
